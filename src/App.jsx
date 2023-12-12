3a5d57139222b1de69cb7eea471e150ca28ae05c
guy-jerome <aaronjamesroberts@gmail.com>
2023-12-12 14:01:44 -0600

    Create the basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import { Paper } from '@mui/material';
+import { Autocomplete, Grid, InputLabel, MenuItem, Paper, Select, TextField } from '@mui/material';
 
-const serviceList = ["Service 1", "Service 2", "Service 3"]
 
 function App() {
-
-
+  const locations = [
+    {label: 'FarmHouse'}
+  ]
   return (
-    <Container >
-
+    <Container>
+      <Box>
+        <Typography variant="h2">Location</Typography>
+        <InputLabel htmlFor="location-name">Name</InputLabel>
+        <TextField id="location-name"></TextField>
+        <InputLabel htmlFor="location-description">Description</InputLabel>
+        <TextField id="location-description"></TextField>
+        <Typography variant='h4'>Create a Path</Typography>
+        <Autocomplete
+          disablePortal
+          id="locations"
+          options={locations}
+          renderInput={(params) => <TextField {...params} label="Locations" />}
+        />
+        <Button>Add</Button>
+      </Box>
+      <Grid></Grid>
     </Container>
   )
 }

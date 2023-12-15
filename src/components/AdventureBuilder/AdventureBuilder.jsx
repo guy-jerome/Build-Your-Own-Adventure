a0ca42b15bd1825b7c08ec4f1813a98c20610d3b
guy-jerome <aaronjamesroberts@gmail.com>
2023-12-14 23:36:45 -0600

    Built out the Adventure Builder

---
+++ src/components/AdventureBuilder/AdventureBuilder.jsx
@@ -0,0 +1,16 @@
+import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
+
+
+export default function AdventureBuilder(){
+
+    return(
+        <Box>
+            <Typography></Typography>
+            <InputLabel></InputLabel>
+            <TextField></TextField>
+            <InputLabel></InputLabel>
+            <TextField></TextField>
+            <Button></Button>
+        </Box>
+    )
+}
